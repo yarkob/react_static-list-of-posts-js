2e819cbcb2ed3ec5034fafe2f3c1feef047f7dbb
yarkob <yaroslav.klark.bondarenko@gmail.com>
2024-02-11 12:14:14 +0200

    Solution v2

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -9,7 +9,7 @@ export const CommentInfo = ({ comment }) => (
 
       <a
         className="CommentInfo__email"
-        href="mailto:Nikita@garfield.biz"
+        href={`mailto:${comment.email}`}
       >
         {comment.email}
       </a>
